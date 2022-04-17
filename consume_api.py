43853e003fbc0f7573b89308d885aa3f86d98360
Giuseppe Falcidia <giuseppefalcidia.mail@gmail.com>
2022-04-17 19:35:20 +0200

    first commit

---
+++ consume_api.py
@@ -0,0 +1,18 @@
+import requests
+
+import json
+
+response = requests.get(
+    'http://api.stackexchange.com/2.2/questions?order=desc&sort=activity&site=stackoverflow')
+
+print(response)
+
+# print(response.json()['items'])
+
+for data in response.json()['items']:
+    if data['answer_count'] == 0:
+        print(data[title])
+        print(data[link])
+    else:
+        print('skipped')
+    print()
